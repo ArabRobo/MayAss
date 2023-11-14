ec699f6c48d800e6ed847486f0f49f108eeed797
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2023-11-13 23:44:43 -0800

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://vxitkksr:L9SUJWZSWs0u81M88CK
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001931180164"))
+FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001607941692"))
 FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "0"))
 FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
